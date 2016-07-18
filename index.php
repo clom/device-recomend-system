9b3221a3f1798f0f993dbaf9339145c30c31bde2
Kento Nomiyama <nonoca@klab.ai.kyutech.ac.jp>
2016-07-18 23:16:28 +0900

    add header

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <html>
-<head>
-	<title> It works!</title>
-</head>
+<?php
+$title = "it works!"; 
+include_once(dirname(__FILE__).'/header.html'); 
+?>
 <body>
-<h1> <?php echo("It works!"); ?> 
+<?php include_once(dirname(__FILE__).'/navbar.html'); ?>  
+<div class="container">
+<h1> <?php echo("It works!"); ?> </h1>
+</div>
 </body>
+</html>
