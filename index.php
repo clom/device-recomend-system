6386ea4fbafc56fff8cdf3912f7f53273b2c636d
Kento Nomiyama <nonoca@klab.ai.kyutech.ac.jp>
2016-07-19 22:56:33 +0900

    view index

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 $title = "it works!"; 
 include_once(dirname(__FILE__).'/header.html'); 
 ?>
-<body>
-<?php include_once(dirname(__FILE__).'/navbar.html'); ?>  
-<div class="container">
-<h1> <?php echo("It works!"); ?> </h1>
-</div>
-</body>
+    <body>
+        <?php include_once(dirname(__FILE__).'/navbar.html'); ?>  
+        <div class="container">
+            <h1> Welcome to Device Recomend Service! </h1>
+            <div class="alert alert-info" role="alert">
+                <p>本サービスは限りなくベータ版です.不具合によるサービス停止もございます.</p>
+            </div>
+        </div>
+    </body>
 </html>
