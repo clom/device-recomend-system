a01927cc53063c03233d5a8be738f138c9daf66f
Kento Nomiyama <nonoca@klab.ai.kyutech.ac.jp>
2016-07-20 21:56:43 +0900

    check acocunt fix

--- lib/adduser.php
+++ lib/adduser.php
@@ -3,7 +3,7 @@
     require_once(dirname(__FILE__)."/../conf.php");
     
     // POST info
-    $id = $_POST['uid'];
+    $id = htmlspecialchars($_POST['uid']);
     $pass = sha1($_POST['password']);
     $passc = sha1($_POST['ckpassword']);
 
@@ -16,9 +16,10 @@
             die('接続失敗です。'.$sqliteerror);
         }
 	    else{
-		    $sql = "select Account_name, Pass_word from auth where Account_name = '".$id."'";
+		    $sql = "select count(*) from auth where Account_name = '".$id."'";
 		    $result = $link->query($sql);
-            if($result->numColumns() > 0){
+            $res = $result->fetchArray(SQLITE3_ASSOC);
+            if($res['count(*)'] == 0){
                 $sql = "insert into auth (Account_name, Pass_word) values ('".$id."','".$pass."')";
 	            $result = $link->exec($sql);
             }
